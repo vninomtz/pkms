2ce292e397bab222d4dcb08f6830a78a0e7df762
Victor Niño <ninomtz.victor@gmail.com>
2025-07-21 22:44:07 -0600

    feat: read note

--- apps/pkms-web/src/SharedNote.tsx
+++ apps/pkms-web/src/SharedNote.tsx
@@ -1,24 +1,87 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useLocation, useParams } from "react-router";
 import { Decrypt } from "./security";
 
-const SharedNote = () => {
+const useResource = (noteId: string, key: string) => {
   const [content, setContent] = useState("");
+  const [error, setError] = useState("");
+  const [isOpen, setOpen] = useState(false);
+
+  const getResource = async () => {
+    const res = await fetch(
+      `${import.meta.env.VITE_API_URL}/resources/${noteId}`,
+    );
+    setOpen(true);
+    if (!res.ok) {
+      setError("Nota no existe o ya ha sido eliminada");
+      return;
+    }
+    const body = await res.json();
+    const value = await Decrypt(body.result, key);
+    setContent(value);
+  };
+
+  return {
+    content,
+    isOpen,
+    error,
+    open: getResource,
+  };
+};
+
+const SharedNote = () => {
   const params = useParams();
   const location = useLocation();
 
-  useEffect(() => {
-    fetch(`${import.meta.env.VITE_API_URL}/resources/${params.noteId}`)
-      .then((res) => res.json())
-      .then((r) => {
-        Decrypt(r?.result, location.hash.slice(1)).then((r) => setContent(r));
-      });
-  }, [params.noteId, location.hash]);
+  const { content, isOpen, error, open } = useResource(
+    params.noteId as string,
+    location.hash.slice(1),
+  );
+
+  if (!isOpen) {
+    return (
+      <div className="bg-neutral-50 text-base">
+        <div className="m-auto w-2/4 h-screen flex justify-center items-center">
+          <button
+            className="bg-white text-gray-800 border border-gray-400 rounded-md px-4"
+            onClick={open}
+          >
+            Open note
+          </button>
+        </div>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="bg-neutral-50 text-base">
+        <div className="m-auto h-screen flex justify-center items-center">
+          {error}
+        </div>
+      </div>
+    );
+  }
+
   return (
-    <div
-      className="overflow-y-auto prose prose-slate text-wrap break-words"
-      dangerouslySetInnerHTML={{ __html: content }}
-    />
+    <div className="bg-neutral-50 text-base">
+      <div className="m-auto h-screen w-2/4 flex justify-center items-center">
+        <TextContent content={content} />
+      </div>
+    </div>
+  );
+};
+
+const TextContent = ({ content }: { content: string }) => {
+  return (
+    <div className="w-full">
+      <textarea
+        className="w-full border border-gray-200 p-2 rounded-md min-h-5"
+        name="notecontent"
+        readOnly
+        value={content}
+      />
+    </div>
   );
 };
 
