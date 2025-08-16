57c920df3e996e75b20b32ecfa89ac8b09e740c6
Victor Niño <ninomtz.victor@gmail.com>
2025-08-16 17:11:15 -0600

    db: update db package

--- database/setup.sql
+++ database/setup.sql
@@ -1,8 +1,6 @@
 DROP TABLE IF EXISTS Nodes;
 
 CREATE TABLE Nodes(
-  Id INTEGER PRIMARY KEY,
-  Title TEXT NOT NULL,
-  Description TEXT,
-  NodeType TEXT
+  Title TEXT NOT NULL PRIMARY KEY,
+  Content TEXT
 )
